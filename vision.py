45b553cc83a770ca9a112407b2e25fb1ec4a63f7
Kevin M Ellis <ellisk@sketch2.csail.mit.edu>
2015-04-21 12:51:19 -0400

    got vision integrated with picture output

--- vision.py
+++ vision.py
@@ -3,21 +3,27 @@ import math
 import sys
 import time
 
-LD = 3 # latent distances
-LA = 1 # latent angles
+LD = 4 # latent distances
+LA = 0 # latent angles
 LS = 1 # latent shapes
 
 '''
 Weird thing: the solver chokes if I don't represent shape constants as real numbers.
 '''
 
-triangle = 42.0
-rectangle = 69.0
+observations = []
+for picture_file in sys.argv[1:]:
+    if picture_file[0] != 'p':
+        picture_file = "pictures/" + picture_file
+    with open(picture_file,'r') as picture:
+        picture = picture.readlines()
+        shapes = eval('['+picture[0]+']')
+        LS = max([LS] + [ shape[3]+1 for shape in shapes ])
+        observations.append([(x,y,s+10.0*len(observations)) for [x,y,sz,s] in shapes ])
+#observations = [ [(3.0*i,4.0*i,triangle) for i in [1,2,3]], #,(3,9,5)],
+#                 [(1.0*i,1.0*i,rectangle) for i in [1,2,3]]] #,(7,10,2)] ]
 
-observations = [ [(3.0*i,4.0*i,triangle) for i in [1,2,3]], #,(3,9,5)],
-                 [(1.0*i,1.0*i,rectangle) for i in [1,2,3]]] #,(7,10,2)] ]
-
-
+dataMDL = len(observations)*(10.0*(LA+LD)+100.0*LS)
     
 def move_turtle(x,y,tx,ty,d,ax,ay):
     constrain(d > 0)
@@ -53,6 +59,9 @@ rule('LOCATE', ['DISTANCE','ORIENTATION'],
 rule('INITIALIZE',[],
      lambda m: "(teleport l[0] l[1])",
      lambda (t,i): (i['distances'][0],i['distances'][1],0.0,1.0))
+rule('INITIAL-SHAPE',[],
+     lambda m: "s[0]",
+     lambda (t,i): i['shapes'][0])
 
 def indexed_shape(j):
     rule('SHAPE',[],
@@ -64,9 +73,10 @@ for j in range(LS): indexed_shape(j)
 def program_generator(d):
     if d < len(observations[0]):
         l, m1, pl = generator(5,'LOCATE')
+        sh, m2, ps = generator(5,'SHAPE')
     else:
         l, m1, pl = generator(5,'INITIALIZE')
-    sh, m2, ps = generator(5,'SHAPE')
+        sh, m2, ps = generator(5,'INITIAL-SHAPE')
     
     rest_of_picture = lambda (t,i): []
     restMDL, restPrint = 0, (lambda m: "")
@@ -105,6 +115,7 @@ for n in range(len(observations)):
     inputs.append(((0,0,1,0),{"distances": ds, "angles": ts, "shapes": ss}))
 
 draw_picture,mdl,pr = program_generator(len(observations[0]))
+mdl = summation([mdl,dataMDL])
 for i in range(len(observations)):
     observation = observations[i]
     picture = draw_picture(inputs[i])
@@ -120,15 +131,16 @@ def full_printer(m):
     program = pr(m)+"\n"
     for n in range(len(observations)):
         program += "\nObservation %i:\n\t" % n
-        for d in inputs[n][1]['distances']:
-            program = program + ("%s = %f; " % (str(d), extract_real(m,d)))
+        for d in range(LD):
+            program = program + ("l[%s] = %f; " % (str(d), extract_real(m,inputs[n][1]['distances'][d])))
         program += "\n\t"
-        for (dx,dy),index in zip(inputs[n][1]['angles'], range(LA)):
-            a = int(math.atan2(extract_real(m,dy),extract_real(m,dx))/math.pi*180.0)
-            program = program + ("a%i[%i] = %i; " % (n, index, a))
-        program += "\n\t"
-        for sh in inputs[n][1]['shapes']:
-            program = program + ("%s = %f; " % (str(sh), extract_real(m,sh)))
+        if LA > 0:
+            for (dx,dy),index in zip(inputs[n][1]['angles'], range(LA)):
+                a = int(math.atan2(extract_real(m,dy),extract_real(m,dx))/math.pi*180.0)
+                program = program + ("a%i[%i] = %i; " % (n, index, a))
+            program += "\n\t"
+        for sh in range(LS):
+            program = program + ("s[%s] = %f; " % (str(sh), extract_real(m,inputs[n][1]['shapes'][sh])))
         program = program + "\n"
     return program
 
@@ -136,4 +148,4 @@ def full_printer(m):
 
 
 
-parallelCompression(full_printer,mdl,[[boolean()]])
+compressionLoop(full_printer,mdl) #,[[boolean()]])
