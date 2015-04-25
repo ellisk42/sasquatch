308cf5bc489b415a3172b462f4b22cda908f1435
kevin <kevin@debian>
2015-04-25 10:32:31 -0700

    coordinate free version of vision

--- vision.py
+++ vision.py
@@ -44,8 +44,8 @@ def move_turtle(x,y,tx,ty,d,ax,ay):
     
     return x+d*nx,y+d*ny,nx,ny
 
-def define_grammar(LD,LA):
-    if LA > 0:
+def define_grammar(LP,LD,LA):
+    if LD > 0:
         rule('ORIENTATION', ['ANGLE'],
              lambda m, a: a,
              lambda i, a: a)
@@ -57,19 +57,19 @@ def define_grammar(LD,LA):
         rule('LOCATE', ['DISTANCE','ORIENTATION'],
              lambda m, d, o: "(move %s %s)" % (d,o),
              lambda ((x,y,dx,dy),i), d, (dxp,dyp): move_turtle(x,y,dx,dy,d,dxp,dyp))
-
-    indexed_rule('DISTANCE', 'l', LD,
-                 lambda (t,i): i['distances'])
+        indexed_rule('DISTANCE', 'l', LD,
+                     lambda (t,i): i['distances'])
     
-    
-    rule('LOCATE', ['DISTANCE','DISTANCE'],
-         lambda m, p, q: "(teleport %s %s)" % (p,q),
-         lambda (t,i), p, q: (p,q,0.0,1.0))
+    indexed_rule('POSITION', 'r', LP,
+                 lambda (t,i): i['positions'])
+    rule('LOCATE', ['POSITION'],
+         lambda m, p: "(teleport %s)" % p,
+         lambda (t,i), (p, q): (p,q,0.0,1.0))
     
     
     rule('INITIALIZE',[],
-         lambda m: "(teleport l[0] l[1])",
-         lambda (t,i): (i['distances'][0],i['distances'][1],0.0,1.0))
+         lambda m: "(teleport r[0])",
+         lambda (t,i): (i['positions'][0][0],i['positions'][0][1],0.0,1.0))
     rule('INITIAL-SHAPE',[],
          lambda m: "s[0]",
          lambda (t,i): i['shapes'][0])
@@ -114,18 +114,22 @@ def check_shape(shape, shapep):
     return [x == xp,y == yp,sh == sp]
 
 solutions = []    
-for LA,LD in [(a,d) for a in [1] for d in [4]]: #range(2,2*len(observations[0])+1) ]:
+for LA,LD,LP in [(a,d,p) for a in [0,1] for d in [0,1,2] for p in range(1,len(observations[0])+1) ]:
+    # make sure that the latent dimensions make sense
+    if LA > 0 and LD == 0: continue
+    if LP + LD > len(observations[0]): continue
     clear_solver()
-    dataMDL = len(observations)*(10.0*(LA+LD)+100.0*LS)
-    define_grammar(LD, LA)
+    dataMDL = len(observations)*(10.0*(LA+LD+2*LP)+100.0*LS)
+    define_grammar(LP, LD, LA)
     inputs = []
     for n in range(len(observations)):
+        ps = [ (real(), real()) for j in range(LP) ]
         ds = real_numbers(LD)
         ts = [ (real(), real()) for j in range(LA) ]
         for tx,ty in ts:
             constrain_angle(tx,ty)
         ss = real_numbers(LS)
-        inputs.append(((0,0,1,0),{"distances": ds, "angles": ts, "shapes": ss}))
+        inputs.append(((0,0,1,0),{"distances": ds, "angles": ts, "shapes": ss, "positions": ps}))
         
     draw_picture,mdl,pr = program_generator(len(observations[0]))
     mdl = summation([mdl,dataMDL])
@@ -152,9 +156,16 @@ for LA,LD in [(a,d) for a in [1] for d in [4]]: #range(2,2*len(observations[0])+
         program = pr(m)+"\n"
         for n in range(len(observations)):
             program += "\nObservation %i:\n\t" % n
-            for d in range(LD):
-                program = program + ("l[%s] = %f; " % (str(d), extract_real(m,inputs[n][1]['distances'][d])))
-            program += "\n\t"
+            for d in range(LP):
+                program = program + ("r[%s] = (%f,%f); " % (str(d),
+                                                            extract_real(m,inputs[n][1]['positions'][d][0]),
+                                                            extract_real(m,inputs[n][1]['positions'][d][1])))
+                program += "\n\t"
+            if LD > 0:
+                for d in range(LD):
+                    program = program + ("l[%s] = %f; " % (str(d), 
+                                                           extract_real(m,inputs[n][1]['distances'][d])))
+                program += "\n\t"
             if LA > 0:
                 for (dx,dy),index in zip(inputs[n][1]['angles'], range(LA)):
                     a = int(math.atan2(extract_real(m,dy),extract_real(m,dx))/math.pi*180.0)
@@ -166,16 +177,16 @@ for LA,LD in [(a,d) for a in [1] for d in [4]]: #range(2,2*len(observations[0])+
         return program
     p,m = compressionLoop(full_printer,mdl)
     if m == None:
-        print "No solution for LA, LD = %i, %i" % (LA,LD)
+        print "No solution for LA, LD, LP = %i, %i, %i" % (LA,LD,LP)
     else:
-        print "Got solution for LA, LD = %i, %i" % (LA,LD)
-        solutions.append((m,p,LA,LD))
+        print "Got solution for LA, LD, LP = %i, %i, %i" % (LA,LD,LP)
+        solutions.append((m,p,LA,LD,LP))
 
 
 
-(m,p,LA,LD) = min(solutions)
+(m,p,LA,LD,LP) = min(solutions)
 print "="*40
-print "Best solution: %f bits (D,A = %i,%i)" % (m,LD,LA)
+print "Best solution: %f bits (D,A,P = %i,%i)" % (m,LD,LA,LP)
 print "="*40
 
 print p
