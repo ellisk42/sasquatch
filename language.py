f457fbf81393450b3fa94d381c29bb717c9a19bb
Kevin M Ellis <ellisk@sketch2.csail.mit.edu>
2015-04-25 15:01:28 -0400

    have demo of it learning everything from a set of minimal pairs

--- language.py
+++ language.py
@@ -215,7 +215,9 @@ observations = [["p e","p e d","p e I N","p e d","p e","p e z"],
                 ["k I k","k I k t","k I k I N","k I k t","k I k","k I k s"],
                 ["E n d","E n d @ d","E n d I N","E n d @ d","E n d","E n d z"],
                 ["b \\ae n","b \\ae n d","b \\ae n I N","b \\ae n d","b \\ae n","b \\ae n z"],
-                ["p a p","p a p t","p a p I N","p a p t","p a p","p a p s"]]
+                ["p a p","p a p t","p a p I N","p a p t","p a p","p a p s"],
+                ["p U S","p U S t","p U S I N","p U S t","p U S","p U S @ z"]]
+N = len(observations)
 latexTable(observations)
 
 maximum_length = max([len(w.split(' ')) for ws in observations for w in ws ])
