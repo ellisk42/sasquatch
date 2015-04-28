cb7895f743055c240348b4aea1244dba575b7c24
kevin <kevin@debian>
2015-04-28 08:57:16 -0700

    added wrapper that turns vision into a classifier

---
+++ classifier.py
@@ -0,0 +1,47 @@
+import random
+import sys
+import subprocess
+
+
+P = int(sys.argv[1]) # which data set are we using
+N = int(sys.argv[2]) # number of positive and negative training examples
+S = int(sys.argv[3]) # number of samples
+
+positives = [ "pictures/%i_1_%i" % (P,x) for x in range(100) ]
+negatives = [ "pictures/%i_0_%i" % (P,x) for x in range(100) ]
+test = ' '.join(positives + negatives)
+
+def run_output(training):
+    o = subprocess.check_output("python vision.py %s test %s" % (training, test), shell = True)
+    likelihoods = []
+    reading_likelihoods = False
+    for l in o:
+        if reading_likelihoods:
+            likelihoods.append(float(l))
+        if "Test data log likelihoods" in l:
+            reading_likelihoods = True
+    return likelihoods[:100], likelihoods[100:]
+
+accuracies = []
+for s in range(S):
+    p = random.sample(positives,N)
+    n = random.sample(negatives,N)
+    
+    pp,pn = run_output(' '.join(p))
+    np,nn = run_output(' '.join(n))    
+    
+    correct = 0
+    for j in range(100):
+        # positive example j
+        if pp[j] > np[j]:
+            correct += 1
+        elif pp[j] == np[j]:
+            correct += 0.5
+        # negative example j
+        if nn[j] > pn[j]:
+            correct += 1
+        elif nn[j] == pn[j]:
+            correct += 0.5
+    print p,n, correct
+    accuracies.append(correct)
+print accuracies
