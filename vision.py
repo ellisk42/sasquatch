f5185292cf44036c3fd3891df1c8c38cee9ce17f
Kevin M Ellis <ellisk@sketch2.csail.mit.edu>
2015-05-04 22:27:38 -0400

    added an extra print statement vision

--- vision.py
+++ vision.py
@@ -298,7 +298,8 @@ for LA,LD,LP in [(a,d,p) for a in [0,1] for d in [0,1,2] for p in range(1,pictur
                 program = program + ("s[%s] = %f; " % (str(sh), extract_real(m,inputs[n][1]['shapes'][sh])))
             program = program + "\n"
         return program
-    p,m = compressionLoop(full_printer,mdl,timeout = solver_timeout*1000)
+    print "Trying LA, LD, LP = %i, %i, %i" % (LA,LD,LP)
+    p,m = compressionLoop(full_printer,mdl,timeout = solver_timeout)
     if m == None:
         print "No solution for LA, LD, LP = %i, %i, %i" % (LA,LD,LP)
     else:
