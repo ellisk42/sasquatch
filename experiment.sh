303cc5067eea9aa4ffce6deeab4f94824ff1bc18
Kevin M Ellis <ellisk@sketch2.csail.mit.edu>
2015-05-06 17:36:52 -0400

    fixed problem and corpus

--- experiment.sh
+++ experiment.sh
@@ -4,16 +4,19 @@ AUTHLOOP=$BIN_DIR/authloop
 AUTHOUT=/tmp/$USER.auth.$$
 
 
-kinit -r 8d
-aklog
-nohup $AUTHLOOP $KEYTAB >> $AUTHOUT &
+#kinit -r 8d
+#aklog
+#nohup $AUTHLOOP $KEYTAB >> $AUTHOUT &
 
 ITERATIONS=$1
 PREFIX=$2
-COMMAND=$3
+shift
+shift
+#COMMAND=$3
 
 for j in `seq 1 $ITERATIONS`;
 do
-    nohup $COMMAND >> $PREFIX$j 2>&1 &
+    longjob -o $PREFIX$j "$@"
+#    nohup $COMMAND >> $PREFIX$j 2>&1 &
 done
 
