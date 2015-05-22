0a40ccbf8b78060ae4287d12668a6cfd94efdc6e
kevin <kevin@debian>
2015-05-22 12:15:20 -0700

    fixed bug in previous commit

--- vision_data.py
+++ vision_data.py
@@ -1,23 +1,24 @@
 from classifier import *
 from dinnerParty import *
+import numpy as np
+
 
 N = int(sys.argv[1]) # number of positive and negative training examples
 S = int(sys.argv[2]) # number of samples
 
-averages = []
-errors = []
 
 def task(t):
     return Bayesian_classifier(t,N,1)
     
-
+def flatten(xs):
+    return [i for l in xs for i in l  ]
 
 T = 23
 
-arguments = [[t]*S for t in range(1,T+1) ]
-arguments = [i for l in arguments for i in l  ]
+arguments = flatten([[t]*S for t in range(1,T+1) ])
 
 a = parallel_map(task, arguments, cores = T)
-a = [eval(x) for x in a ]
-a = np.array(a).reshape((T,S)).tolist()
+a = flatten([eval(x) for x in a ])
+
+a = np.array(a).reshape((T,len(a)/T)).tolist()
 print a
