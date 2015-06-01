80876cfc6869c17c067d97148aaba94d29649cb0
Kevin M Ellis <ellisk@sketch2.csail.mit.edu>
2015-06-01 09:48:39 -0400

    added curves

--- loop_language.py
+++ loop_language.py
@@ -4,4 +4,4 @@ import os
 N = int(sys.argv[1])
 L = sys.argv[2]
 for n in range(N):
-    os.system("python language.py %s coupled" % L)
+    os.system("longjob  -o random_sample/%s/%s python language.py %s lexicon" % (L,n,L))
