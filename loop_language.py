9615af935974e9c993cb5ff0ca342d820eafcebd
Kevin M Ellis <ellisk@sketch2.csail.mit.edu>
2015-06-02 23:10:22 -0400

    foo

--- loop_language.py
+++ loop_language.py
@@ -4,4 +4,4 @@ import os
 N = int(sys.argv[1])
 L = sys.argv[2]
 for n in range(N):
-    os.system("longjob  -o random_sample/2%s/%s python language.py %s lexicon" % (L,n,L))
+    os.system("longjob  -o random_sample/%s/%s python language.py %s lexicon" % (L,n,L))
